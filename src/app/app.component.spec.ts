d57d4c005804fe672f5ffc11fbe5439fc7a08a05
Student CL7-28 <cl7-28@cl7.lab>
2024-03-18 12:49:26 +0800

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,30 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+    });
+  }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'midtermexam'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('midtermexam');
+  it(`should have as title 'app works!'`, () => {
+    expect(component.title).toEqual('app works!');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should render title in a h1 tag', () => {
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, midtermexam');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('app works!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
 
+interface Employee {
+  EmployeeId: string;
+  EmployeeNumber: string;
+  FirstName: string;
+  LastName: string;
+  Birthday: string;
+  Gender: string;
+  Picture: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'midtermexam';
+  title = 'ITEWT MIDTERM EXAM';
+  employees: Employee[] = [
+    { EmployeeId: "22", EmployeeNumber: "22", FirstName: "John", LastName: "Doe", Birthday: "2002-01-01", Gender: "M", Picture: "https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png"},
+
+  ];
+  model: Partial<Employee> = {};
+  model2: Partial<Employee> = {};
+  msg = "";
+
+  addEmployee(): void {
+    this.employees.push(this.model as Employee);
+    this.model = {};
+    this.msg = "The record has been successfully added.";
+  }
+
+  deleteEmployee(i: number): void {
+    this.employees.splice(i, 1);
+    this.msg = "The record has been successfully deleted.";
+  }
+
+  myValue: number;
+  editEmployee(k: number): void {
+    this.model2 = { ...this.employees[k] };
+    this.myValue = k;
+  }
+
+  updateEmployee(): void {
+    const k = this.myValue;
+    for (let i = 0; i < this.employees.length; i++) {
+      if (i === k) {
+        this.employees[i] = this.model2 as Employee;
+        this.model2 = {};
+        this.msg = "The record has been successfully updated.";
+      }
+    }
+  }
+
+  clickMe(): void {
+    this.msg = "";
+  }
 }
